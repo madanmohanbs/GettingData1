aac80da6344a468e2e030f148e78e2a972e66a46
madanmohan <madanmohanbs@gmail.com>
2014-10-27 03:42:21 +0530

    draft

---
+++ run_analysis.R
@@ -0,0 +1,8 @@
+myHist <- function(mu){
+    hist(galton$child, col="blue", breaks=100)
+    lines(c(mu,mu), c(0,150), col="red",lwd=5)
+    mse <-mean((galton$child -mu)^2)
+    text(63,150, paste("mu=", mu))
+    text(63,140, paste("MSE=", round(mse,2)))
+}
+
